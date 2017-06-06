2b9c3b8bd2af5ffebe9ce08ccac79332a0b92127
Boris Zaydlin <boris@compucorp.co.uk>
2017-06-06 18:00:26 +0300

    PGW-10: Layouts added for most paragraph bundles. Some base theming adjustments. Some elements styled

---
+++ sites/all/themes/tpg_theme/ds_layouts/full_width_narrow/full-width-narrow.tpl.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @file
+ * Display Suite Full width narrow template.
+ *
+ * Available variables:
+ *
+ * Layout:
+ * - $classes: String of classes that can be used to style this layout.
+ * - $contextual_links: Renderable array of contextual links.
+ * - $layout_wrapper: wrapper surrounding the layout.
+ *
+ * Regions:
+ *
+ * - $center: Rendered content for the "Center" region.
+ * - $center_classes: String of classes that can be used to style the "Center" region.
+ */
+?>
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="container <?php print $classes;?> clearfix">
+  <div class="row">
+
+    <<?php print $sidebar_first_wrapper; ?> class="ds-sidebar-first col-md-3<?php print $sidebar_first_classes; ?>">
+      <?php print $sidebar_first; ?>
+    </<?php print $sidebar_first_wrapper; ?>>
+
+    <div class="col-md-6">
+      <!-- Needed to activate contextual links -->
+      <?php if (isset($title_suffix['contextual_links'])): ?>
+        <?php print render($title_suffix['contextual_links']); ?>
+      <?php endif; ?>
+
+      <<?php print $center_wrapper; ?> class="ds-center<?php print $center_classes; ?>">
+        <?php print $center; ?>
+      </<?php print $center_wrapper; ?>>
+    </div>
+
+    <<?php print $sidebar_second_wrapper; ?> class="ds-sidebar-second col-md-3<?php print $sidebar_second_classes; ?>">
+      <?php print $sidebar_second; ?>
+    </<?php print $sidebar_second_wrapper; ?>>
+  </div>
+</<?php print $layout_wrapper ?>>
+
+<!-- Needed to activate display suite support on forms -->
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
