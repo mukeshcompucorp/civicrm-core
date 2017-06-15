535e619e701d3e23f140d7c5494b7704af6d2ac4
Hitesh Jain <hitesh@compucorp.co.uk>
2017-06-15 13:39:04 +0530

    PGW-11: Related activities fix and recreated event for field.

--- sites/all/modules/custom/tpg_global/js/tpg_global.js
+++ sites/all/modules/custom/tpg_global/js/tpg_global.js
@@ -2,7 +2,39 @@
 
   Drupal.behaviors.global_js = {
     attach: function (context,settings) {
+    	
 
+    	if ($('input.daterangepicker').length){
+
+	    	$('input.daterangepicker').each(function(index, element) {
+	    		
+	    		// Enable date range for all inputs with the given class.
+		        $(this).daterangepicker({
+		          initialText: settings.daterangepicker.initialText,
+		          applyButtonText: settings.daterangepicker.applyButtonText,
+		          clearButtonText: settings.daterangepicker.clearButtonText,
+		          cancelButtonText: settings.daterangepicker.cancelButtonText,
+		          rangeSplitter: settings.daterangepicker.rangeSplitter,
+		          dateFormat: settings.daterangepicker.dateFormat,
+		          altFormat: settings.daterangepicker.altFormat,
+		          datepickerOptions : {
+		            numberOfMonths: settings.daterangepicker.datepickerOptions.numberOfMonths
+		          },
+		         	presetRanges: [
+						{text: 'Today', dateStart: function() { return moment() }, dateEnd: function() { return moment() } },
+						{text: 'This week', dateStart: function() { return moment().day(1) }, dateEnd: function() { return moment().day(7) } },
+						{text: 'This weekend', dateStart: function() { return moment().day(6) }, dateEnd: function() { return moment().day(7) } },
+						{text: 'Next month', dateStart: function() { return moment().month() }, dateEnd: function() { return moment().month() } },
+					], 
+				 	datepickerOptions: {
+						maxDate: '+1Y',
+						yearRange: "-20:+2",
+						firstDay: 1,
+					}
+		        });
+
+	    	});
+	    }
     }
   };
 })(jQuery);
