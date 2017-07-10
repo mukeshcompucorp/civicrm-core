fe1a1a317e266ca9ac0fcf8f787d6576a2e426dc
Boris Zaydlin <boris@compucorp.co.uk>
2017-07-10 10:06:15 +0300

    PGW-29: Header bg updated, datepicker adjustments

--- sites/all/modules/custom/tpg_global/js/tpg_global.js
+++ sites/all/modules/custom/tpg_global/js/tpg_global.js
@@ -28,6 +28,11 @@
         $('input.daterangepicker').each(function(index, element) {
           // Enable date range for all inputs with the given class.
           $(this).daterangepicker({
+            onOpen: function(event, data) {
+              if (!$('#pick-a-date').length) {
+                $('.comiseo-daterangepicker-presets > .ui-widget').after('<span id="pick-a-date">' + Drupal.t('Pick a date') + '</span>');
+              }
+            },
             initialText: settings.daterangepicker.initialText,
             applyButtonText: settings.daterangepicker.applyButtonText,
             clearButtonText: settings.daterangepicker.clearButtonText,
@@ -43,7 +48,6 @@
               {text: 'This Weekend', dateStart: function() { return moment().day(6) }, dateEnd: function() { return moment().day(7) } },
               {text: 'This Week', dateStart: function() { return moment().day(1) }, dateEnd: function() { return moment().day(7) } },
               {text: 'Next Month', dateStart: function() { return moment().add(1, 'month').startOf('month') }, dateEnd: function() { return moment().add(1, 'month').endOf('month') } },
-              {text: '<span id="pick-a-date">' + Drupal.t("Pick a date") + '</span>'},
             ],
             datepickerOptions: {
               maxDate: '+1Y',
