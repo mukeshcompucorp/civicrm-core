752546010bc4f9f7948d7328fc49016fbcf0d8d9
Boris Zaydlin <boris@compucorp.co.uk>
2017-07-17 12:13:11 +0300

    PGW-29: Set default value to input

--- sites/all/themes/tpg_theme/js/daterangepicker.js
+++ sites/all/themes/tpg_theme/js/daterangepicker.js
@@ -3,23 +3,32 @@
 
   Drupal.behaviors.dateRangePicker = {
     attach: function(context, settings) {
-      $('input#daterangepicker', context).once().daterangepicker({
-        autoApply: true,
-        parentEl: '.daterangepicker-wrapper',
-        locale: {
-          customRangeLabel: Drupal.t('Pick a date')
-        },
-        ranges: {
-           'Today': [moment(), moment()],
-           'This Weekend': [moment().day(6), moment().day(7)],
-           'This Week': [moment().day(1), moment().day(7)],
-           'Next Month': [moment().add(1, 'month').startOf('month'), moment().add(1, 'month').endOf('month')],
+      var $datepickerInput = $('input#daterangepicker', context);
+      if ($datepickerInput.length) {
+        $datepickerInput.once().daterangepicker({
+          autoApply: true,
+          parentEl: '.daterangepicker-wrapper',
+          locale: {
+            customRangeLabel: Drupal.t('Pick a date')
+          },
+          ranges: {
+             'Today': [moment(), moment()],
+             'This Weekend': [moment().day(6), moment().day(7)],
+             'This Week': [moment().day(1), moment().day(7)],
+             'Next Month': [moment().add(1, 'month').startOf('month'), moment().add(1, 'month').endOf('month')],
+          }
+        }).on('show.daterangepicker', function(ev, picker) {
+          $('.daterangepicker-input', context).addClass('opened');
+        }).on('hide.daterangepicker', function(ev, picker) {
+          $('.daterangepicker-input', context).removeClass('opened');
+        });
+
+        window.onload=function(){
+          if ($datepickerInput.attr('value').length) {
+            $datepickerInput.val($datepickerInput.attr('value'));
+          }
         }
-      }).on('show.daterangepicker', function(ev, picker) {
-        $('.daterangepicker-input', context).addClass('opened');
-      }).on('hide.daterangepicker', function(ev, picker) {
-        $('.daterangepicker-input', context).removeClass('opened');
-      });
+      }
     },
   };
 
