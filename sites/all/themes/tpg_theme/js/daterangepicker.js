d2a3a9b01131e3081e771373e196b6858be9c197
Boris Zaydlin <boris@compucorp.co.uk>
2017-07-17 11:57:29 +0300

    PGW-29: Datepicker calendar styles added

--- sites/all/themes/tpg_theme/js/daterangepicker.js
+++ sites/all/themes/tpg_theme/js/daterangepicker.js
@@ -3,15 +3,22 @@
 
   Drupal.behaviors.dateRangePicker = {
     attach: function(context, settings) {
-
       $('input#daterangepicker', context).once().daterangepicker({
         autoApply: true,
+        parentEl: '.daterangepicker-wrapper',
+        locale: {
+          customRangeLabel: Drupal.t('Pick a date')
+        },
         ranges: {
            'Today': [moment(), moment()],
            'This Weekend': [moment().day(6), moment().day(7)],
            'This Week': [moment().day(1), moment().day(7)],
            'Next Month': [moment().add(1, 'month').startOf('month'), moment().add(1, 'month').endOf('month')],
         }
+      }).on('show.daterangepicker', function(ev, picker) {
+        $('.daterangepicker-input', context).addClass('opened');
+      }).on('hide.daterangepicker', function(ev, picker) {
+        $('.daterangepicker-input', context).removeClass('opened');
       });
     },
   };
